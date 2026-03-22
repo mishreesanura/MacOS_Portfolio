f88d943613467159943f019453af779693fc5b6a
Mishree Sanura <mishree.sanura270@svkmmumbai.onmicrosoft.com>
2026-03-22 15:31:40 +0530

    resolved errors

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -8,7 +8,7 @@ import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
-).toString();
+
 
 const Resume = () => {
   return (
